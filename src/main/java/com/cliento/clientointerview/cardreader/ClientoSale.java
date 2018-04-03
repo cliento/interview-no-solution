66789899d795bc1d11449f3b75c3adc3645c4032
John <john.blomstedt@cargoit.se>
2018-04-03 15:27:59 +0200

    Add missing transactional annotation

--- src/main/java/com/cliento/clientointerview/cardreader/ClientoSale.java
+++ src/main/java/com/cliento/clientointerview/cardreader/ClientoSale.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.cliento.clientointerview.jdbc.CashRegisterStorage;
 import com.cliento.clientointerview.restapi.Sale;
@@ -16,6 +17,7 @@ public class ClientoSale implements SaleService{
 	CashRegisterStorage cashRegisterStorage;
 
 	@Override
+	@Transactional
 	public Optional<SaleResponse> doSale(Sale sale, CardTransaction cardTransaction) {
 		
 		long customerId = cashRegisterStorage.createCustomer(sale.getCustomer().getName(), sale.getCustomer().getEmail());
